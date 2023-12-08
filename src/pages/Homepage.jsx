1b0f4f58218b777bd559142f2654d3dd37377024
Punya Prashun <punya.prasun.cs24@iilmcet.ac.in>
2023-12-08 20:10:14 +0530

    added pages and comp

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navbar, Footer } from '../components';
+const Homepage = () => {
+
+return (
+    <>
+      <Navbar/>
+      <Footer/>
+    </>
+  );
+};
+
+export default Homepage;
