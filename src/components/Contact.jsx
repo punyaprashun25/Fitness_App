bd744f312fecfe514022b725c8b62e8dfa4fbe24
Punya Prashun <punya.prasun.cs24@iilmcet.ac.in>
2023-12-11 00:57:08 +0530

    contact section in progress

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
-
+import './contact.css'
+import Hero from '../assets/gallery2.png'
 const Contact = () => {
   return (
-    <div>
-      
+    <div className="contact-container">
+      <div className="left-side">
+        <div className="head-text">
+          <div className="text-heading">
+            <div className="line"></div>
+            <p className="heading">Contact Us</p>
+          </div>
+        </div>
+        <div className="contact-box">
+          <p className="details"><span className='bold-text'>Address : </span>Knowledge Park-III, Greater Noida, Uttar Pradesh, 201308</p>
+          <p className="details"><span className='bold-text'>Email : </span>punya.prasun.cs24@iilmcet.ac.in</p>
+        </div>
+      </div>
+      <div className="right-side">
+        <img src={Hero} alt="" />
+      </div>
     </div>
+
   )
 }
 
